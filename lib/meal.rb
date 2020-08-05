e4c604c2bbecc371911a078f53ff29c6044e9839
Warda Ayaz <wayaz107@gmail.com>
2020-08-05 04:56:00 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -19,7 +19,10 @@ def meals
 end
 
 def best_tipper
-best_tipped_meal = meals.max {|meal_a, meal_b| meal_a.tip <=> meal_b.tip}
+best_tipped_meal = meals.max do |meal_a, meal_b|
+  meal_a.tip <=> meal_b.tip
+end 
+
 best_tipped_meal.customer
 end
 
