48ae37ac46aedce518921e2e2106511892cbe5bb
Warda Ayaz <wayaz107@gmail.com>
2020-08-05 04:57:20 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -14,17 +14,6 @@ def self.all
   @@all
 end
 
-def meals
-  Meal.all.select {|meal|meal.waiter ==self}
-end
-
-def best_tipper
-best_tipped_meal = meals.max do |meal_a, meal_b|
-  meal_a.tip <=> meal_b.tip
-end 
-
-best_tipped_meal.customer
-end
 
 
 
