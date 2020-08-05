ba85c99ba797aa21c3505bda44bec084ff2bde8f
Warda Ayaz <wayaz107@gmail.com>
2020-08-05 04:45:25 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,24 @@
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_meal(waiter,total, tip = 0)
+  Meal.new(waiter,self,total,tip)
+end
+
+
+
+
+
 end
